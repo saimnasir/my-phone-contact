3e84aaeaf23df6c9386d2348ac6a6f8dbe23d3d9
saimnasir <saim.nasir.65@gmail.com>
2020-11-29 16:41:21 +0300

    api client, contract, event q first setup

---
+++ PhoneContact/PhoneContact.API.Client/Resources/PhoneContactContactResource.cs
@@ -0,0 +1,31 @@
+﻿using PhoneContact.API.Client.Base;
+using PhoneContact.API.Contract.Item;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhoneContact.API.Client.Resources
+{
+    public class PhoneContactContactResource : IPhoneContactContactResource
+    {
+        private readonly IBaseClient _client;
+
+        public PhoneContactContactResource(IBaseClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<ContactResponse> Get(Guid id, CancellationToken cancellationToken = default)
+        {
+            var uri = BuildUri(id);
+            return await _client.GetAsync<ContactResponse>(uri, cancellationToken);
+        }
+
+        private Uri BuildUri(Guid id, string path = "")
+        {
+            return _client.BuildUri(string.Format("api/contacts/{0}", id, path));
+        }
+    }
+}
---
+++ PhoneContact/PhoneContact.API.Contract/Item/ContactResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneContact.API.Contract.Item
+{
+    public class ContactResponse : BaseResponse
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
