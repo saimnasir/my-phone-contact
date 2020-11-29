967ee80bd5c56fc9ac03d25fe3d0ff8cd9d5861a
saimnasir <saim.nasir.65@gmail.com>
2020-11-29 16:38:57 +0300

    phone contact domain

---
+++ PhoneContact/PhoneContact.Domain/Entities/ContactInfo.cs
@@ -0,0 +1,14 @@
+﻿using PhoneContact.Domain.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneContact.Domain.Entities
+{
+    public class ContactInfo : BaseModel
+    {
+        public long Contact { get; set; }
+        public InfoType InfoType { get; set; }
+        public string Information { get; set; }
+    }
+}
